e083f95c356015d4049031aaca19f4ebe748d5f3
Spencer-26p <spencer1997@byui.edu>
2021-05-10 21:57:16 -0500

    Adding the core script file to the repository,

---
+++ hello-world.java
@@ -0,0 +1,12 @@
+package hello-world;
+
+public class HelloWorld {
+
+    public void sayHello() {
+        System.out.println("Hello World!");
+    }
+
+    public static void main(String[] args) {
+        new HelloWorld().sayHello();
+    }
+}
