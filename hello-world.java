e461522ee673e5c5a0dc704fff6e4474f671738d
Spencer-26p <spencer1997@byui.edu>
2021-05-10 22:01:28 -0500

    Added new words

--- hello-world.java
+++ hello-world.java
@@ -3,7 +3,7 @@ package hello-world;
 public class HelloWorld {
 
     public void sayHello() {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! Wow this is great!");
     }
 
     public static void main(String[] args) {
